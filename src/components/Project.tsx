6fc6419edb2c26ddd2b72b3c3c6420286eaf7b34
Barath Charles <barathclm@icloud.com>
2026-04-07 22:47:40 +0530

    project > added re-entry sim

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,6 +9,8 @@ import mock07 from '../assets/images/mock07.png';
 import mock08 from '../assets/images/mock08.png';
 import mock09 from '../assets/images/mock09.png';
 import mock10 from '../assets/images/mock10.png';
+import eng01 from '../assets/images/engineering/reentry_plot.png';
+
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -17,7 +19,7 @@ function Project() {
         <h1>Project Insights</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={eng01} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Filmate AI</h2></a>
                 <p>Developed movie finder app with semantic search and sentiment analysis using OpenAI GPT-3.5 Turbo, Qdrant, React, and Flask.</p>
             </div>
