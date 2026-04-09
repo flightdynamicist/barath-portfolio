bc807dc253991c5aafbc7cf18fbca63e2957c932
Barath Charles <barathclm@icloud.com>
2026-04-09 16:01:46 +0530

    minor fix for image resolutions

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import mock01 from '../assets/images/mock01.png';
-import mock02 from '../assets/images/mock02.png';
-import mock03 from '../assets/images/mock03.png';
-import mock04 from '../assets/images/mock04.png';
-import mock05 from '../assets/images/mock05.png';
-import mock06 from '../assets/images/mock06.png';
-import mock07 from '../assets/images/mock07.png';
-import mock08 from '../assets/images/mock08.png';
-import mock09 from '../assets/images/mock09.png';
-import mock10 from '../assets/images/mock10.png';
 import eng01 from '../assets/images/engineering/reentry_plot_watermark.png';
 import eng02 from '../assets/images/engineering/earth-moon-transfer_watermark.png';
+import eng03 from '../assets/images/engineering/rendezvous_docking_watermark.png';
 
 import '../assets/styles/Project.scss';
 
@@ -30,6 +21,11 @@ function Project() {
                 <img src={eng02} className="zoom" alt="thumbnail" width="100%"/>
                 <h2>Cislunar Transfer Trajectory Architecture</h2>
                 <p> Earth–Moon transfer, capturing injection, transit, and lunar approach phases. Colored trajectories represent guidance-driven path evolution under gravitational dynamics. From orbit design to terminal approach — a glimpse into precision GNC in deep space</p>
+            </div>
+            <div className="project">
+                <img src={eng03} className="zoom" alt="thumbnail" width="100%"/>
+                <h2>Autonomous Rendezvous and Docking System</h2>
+                <p> The simulation illustrates a controlled approach toward a target vehicle, including relative motion dynamics, velocity reduction, and final alignment for safe capture. From long-range phasing to proximity operations — this highlights precision control in orbital environments.</p>
             </div>
         </div>
     </div>
